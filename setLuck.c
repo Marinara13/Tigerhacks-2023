6a4ddbe60e9e276a94b6c3e22e4e386f0bf4ab8b
AmPotatoChip <130861066+AmPotatoChip@users.noreply.github.com>
2023-11-04 13:59:04 -0500

    Update setLuck.c

--- setLuck.c
+++ setLuck.c
@@ -1,16 +1,13 @@
-#include <time.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "header.h"
 
 // returns a random integer between 0 and 100
-// DON'T CALL MULTIPLE TIMES SIMULTANEOUSLY
 int setLuck(position current)
 {
     time_t t;
-    int counter = *(current.luckCounter)
+    int counter = *(current.luckCount);
     srand((unsigned) time(&t)*counter);
     int luck = (rand())%101;
     counter++;
-    *(current.luckCounter) = counter;
+    *(current.luckCount) = counter;
     return luck;
 }
