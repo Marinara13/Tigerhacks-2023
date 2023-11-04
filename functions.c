63a6a731cbab2b840e4cac2c1d1e19119925d50a
AmPotatoChip <130861066+AmPotatoChip@users.noreply.github.com>
2023-11-04 16:38:25 -0500

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,7 +1,30 @@
 #include "header.h"
 
 
+void audition(position current){
+    char opt1[]=" prompt 1";
+    char opt2[]=" prompt 2 ";
+    char opt3[]=" prompt 3";
 
+
+    setOpt(current, opt1, opt2, opt3, NULL);
+    printOpt(current);
+    choice3( current)
+    
+    switch (*(current.action)) {
+        case 1:
+            //option 1
+            break;
+        case 2:
+            //option 2
+            break;
+        case 3:
+            //option 3
+            break;
+    }
+
+    
+}
 
 void mainChoice(position current) {
     char opt1[]=" Audition for a roll";
