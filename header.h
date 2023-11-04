47eda1e9de24cad56366963cfbfb238b2b384cb7
AmPotatoChip <130861066+AmPotatoChip@users.noreply.github.com>
2023-11-04 13:11:53 -0500

    Update header.h

--- header.h
+++ header.h
@@ -24,5 +24,6 @@ typedef struct{
 position createPlayer();
 void setOpt(position current,char*opt1,char*opt2,char*opt3,char*opt4 );
 void printOpt(position current);
-void choice(position current);
+void choice2(position current);
+void choice4(position current);
 void freePositon(position current);
