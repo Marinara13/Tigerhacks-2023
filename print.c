43c0fb795b049eeb4251840f814337e565f86b06
AmPotatoChip <130861066+AmPotatoChip@users.noreply.github.com>
2023-11-04 14:05:52 -0500

    Update print.c

--- print.c
+++ print.c
@@ -140,3 +140,15 @@ void printStats(position current) {
     printf("Net Worth: %d\n", *(current.networth));
     
 }
+
+// returns a random integer between 0 and 100
+int setLuck(position current)
+{
+    time_t t;
+    int counter = *(current.luckCounter);
+    srand((unsigned) time(&t)*counter);
+    int luck = (rand())%101;
+    counter++;
+    *(current.luckCounter) = counter;
+    return luck;
+}
