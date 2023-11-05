5757304bdc72bb2865030b17ec77fbd293f09545
AmPotatoChip <130861066+AmPotatoChip@users.noreply.github.com>
2023-11-04 22:19:08 -0500

    Update main.c

--- main.c
+++ main.c
@@ -3,11 +3,14 @@
 int main (void)
 {
     position player = storyIntro();
+    sleep(3);
     printStats(player);
+    sleep(3);
     location(player);
     while (*(player.age) != 22) {
         mainChoice(player);
         printStats(player);
+        sleep(3);
         randomEvent(player);
         if(*(player.fail) == 1){
             break;
